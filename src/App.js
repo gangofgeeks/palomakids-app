a9a6b8ed83fd6a7f07360a387c7dd7386d01ea57
mohit srivastava <mohitsrivastava@mohits-Air.attlocal.net>
2023-02-13 23:58:38 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Check check check
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import axios from 'axios';
+class App extends Component {
+  componentDidMount() {
+    const api = 'https://53za4egyyc.execute-api.us-east-2.amazonaws.com/undefined/';
+    const data = { "name" : "Mike" };
+    axios
+      .post(api, data)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    }
+  render() {
+    return (
+      <div>Medium Tutorial</div>
+    );
+  }
 }
-
 export default App;
